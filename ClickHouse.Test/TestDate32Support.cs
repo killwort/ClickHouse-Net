ef397a614118b8be3be584765c15a92d1ca70eff
dorzhevsky <d.orzhevsky@gardatech.ru>
2022-09-26 16:45:01 +0300

    Tests refactoring

--- ClickHouse.Test/TestDate32Support.cs
+++ ClickHouse.Test/TestDate32Support.cs
@@ -18,52 +18,56 @@ namespace ClickHouse.Test
                 cnn.CreateCommand("DROP TABLE IF EXISTS test_date32").ExecuteNonQuery();
                 cnn.CreateCommand("CREATE TABLE test_date32 (date32_column Date32)  ENGINE = MergeTree ORDER BY date32_column SETTINGS index_granularity = 8192").ExecuteNonQuery();
             }
-
-            Thread.Sleep(1000);
         }
 
         [Test]
         public void TestInsertBulk()
         {
+            var value = new DateTime(2000, 01, 02);
             using (var cnn = ConnectionHandler.GetConnection())
             {
-                cnn.CreateCommand("INSERT INTO test_date32 (date32_column) VALUES @bulk").AddParameter("bulk", DbType.Object, new object[] { new object[] { DateTime.Now } })
-                   .ExecuteNonQuery();
+                cnn.CreateCommand("INSERT INTO test_date32 (date32_column) VALUES @bulk").AddParameter("bulk", DbType.Object, new object[] { new object[] { value } })
+                    .ExecuteNonQuery();
             }
+            var values = SelectValues();
+            Assert.AreEqual(value, values[0]);            
         }
 
         [Test]
         public void TestInsertLiteral()
         {
+            var value = new DateTime(2000, 01, 02);
             using (var cnn = ConnectionHandler.GetConnection())
             {
-                cnn.CreateCommand("INSERT INTO test_date32 (date32_column) VALUES ('2020-01-01')").ExecuteNonQuery();
+                cnn.CreateCommand($"INSERT INTO test_date32 (date32_column) VALUES ('{value.ToString("yyyy-MM-dd")}')").ExecuteNonQuery();
             }
+            var values = SelectValues();
+            Assert.AreEqual(value, values[0]);            
         }
 
         [Test]
         public void TestInsertLiteralParameter()
         {
+            var value = new DateTime(2000, 01, 02);
             using (var cnn = ConnectionHandler.GetConnection())
             {
-                cnn.CreateCommand("INSERT INTO test_date32 (date32_column) VALUES (@p1)").AddParameter("p1", DbType.DateTime, DateTime.Now).ExecuteNonQuery();
+                cnn.CreateCommand("INSERT INTO test_date32 (date32_column) VALUES (@p1)").AddParameter("p1", DbType.DateTime, value).ExecuteNonQuery();
             }
+            var values = SelectValues();
+            Assert.AreEqual(value, values[0]);            
         }
 
-        [Test]
-        public void TestSelect()
+        private List<DateTime> SelectValues()
         {
             using (var cnn = ConnectionHandler.GetConnection())
             {
-                cnn.CreateCommand("INSERT INTO test_date32 (date32_column) VALUES (@p)")
-                   .AddParameter("p", DbType.DateTime, new DateTime(2000, 01, 02))
-                   .ExecuteNonQuery();
                 var values = new List<DateTime>();
                 using (var cmd = cnn.CreateCommand("SELECT date32_column FROM test_date32"))
                 using (var reader = cmd.ExecuteReader())
                 {
                     reader.ReadAll(r => { values.Add(r.GetDateTime(0)); });
                 }
+                return values;
             }
         }
     }
